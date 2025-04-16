90d9df48d7fe4e5647e20e8357f70131e4271233
RisanNasir <62825093+RisanNasir@users.noreply.github.com>
2025-04-16 06:01:47 +0100

    Add files via upload

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var buttons = document.querySelectorAll(".drum");
 var audio;
 for(var i = 0; i < buttons.length; i++){
@@ -43,4 +44,51 @@ function playSound(c){
         default:
 
     }
+=======
+var buttons = document.querySelectorAll(".drum");
+var audio;
+for(var i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", function (){
+        playSound(this.innerText);
+    });
+}
+
+    document.addEventListener("keyup", function (e) {
+        playSound(e.key);
+    });
+
+function playSound(c){
+    switch (c) {
+        case 'w':
+            audio = new Audio('./sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case 'a':
+            audio = new Audio('./sounds/tom-2.mp3');
+            audio.play()
+            break;
+        case 's':
+            audio = new Audio('./sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case 'd':
+            audio = new Audio('./sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case 'j':
+            audio = new Audio('./sounds/snare.mp3');
+            audio.play();
+            break;
+        case 'k':
+            audio = new Audio('./sounds/crash.mp3');
+            audio.play();
+            break;
+        case 'l':
+            audio = new Audio('./sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        default:
+
+    }
+>>>>>>> 85e0f4ccb9fa554fd050169bb4dd8ee345886c1d
 }
